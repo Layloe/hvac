088686ffcd2bce556ff2872be5b6f9f6fb95c919
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-09-22 10:47:25 -0700

    save

--- routes/router.js
+++ routes/router.js
@@ -5,4 +5,15 @@ const path = require('path')
 
 router.post('/schedule_service', async (rec, res) => {
     const {name, email, phone, date} = req.body
+
+    try {
+        const newAppointment = new Appointment({name, email, phone, date})
+        await newAppointment.save()
+        res.sendFile(path.join(__dirname, '../views/bookResults.html'))
+    } catch (err) {
+        console.log(err)
+        res.sendFile(path.join(__dirname, '../views/bookError.html'))
+    }
 })
+
+module.exports = router 
