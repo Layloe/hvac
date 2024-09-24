24288ea7ca3b4152cf3ab1bf18a3122997ee1803
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-09-24 06:12:09 -0700

    quote form build

--- routes/router.js
+++ routes/router.js
@@ -22,7 +22,7 @@ const sender = {
   };
 
 router.post('/schedule_service', async (req, res) => {
-    const {name, email, phone, date} = req.body;
+    const { name, email, phone, date } = req.body;
 
     let scheduleEmailText = `We've received your request to schedule an appointment on ${date}. 
 We'll give you a call at ${phone} to find a time that works for you.
@@ -46,6 +46,35 @@ Lozano Brothers HVAC Team`
     console.log(err)
     res.sendFile(path.join(__dirname, '../views/bookError.html'))
     }
+})
+
+router.post('/request_quote', async (req, res) => {
+  const { name, email, service, message } = req.body;
+
+  let quoteEmailText = `Dear ${name},
+We're honored that you're considering us for your ${service} needs!
+One of our knowledgeable reps will email you at this address within one business day to hear more about how we can assist you.
+For your records, your message to us is below:
+
+${message}
+
+Sincerely,
+Lozano Brothers HVAC Team`
+
+  try {
+      transport.sendMail({
+      from: sender,
+      to: email,
+      subject: "HVAC Quote Request",
+      text: quoteEmailText,
+      category: "Integration Test",
+})
+
+  res.sendFile(path.join(__dirname, '../views/bookResult.html'))
+  } catch (err) {
+  console.log(err)
+  res.sendFile(path.join(__dirname, '../views/bookError.html'))
+  }
 })
 
 module.exports = router 
